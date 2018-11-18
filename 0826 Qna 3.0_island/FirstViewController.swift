6f99647964124a818c9b59981a579d9a48491121
moon0411 <33474743+moon0411@users.noreply.github.com>
2018-11-18 15:54:45 +0800

    Update FirstViewController.swift
    array change

--- 0826 Qna 3.0_island/FirstViewController.swift
+++ 0826 Qna 3.0_island/FirstViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class FirstViewController: UIViewController {
     var count = 0
-    var questions: [Question] = [Question(question: "請問唐伯虎以西洋打擊樂器打動華夫人，最後終得順利成為書僮？", answer: "o"), Question(question:"華安在華太師府的終身代號是1314?", answer: "x"), Question(question: "對穿腸，綽號「對王之王」，名片上的職稱是七省文狀元兼參謀將軍？", answer: "o"),Question(question: "含笑半步顛是用蜂蜜,川貝,桔梗,加上清新白蓮配製而成", answer: "x"), Question(question: "吃了一日喪命散的人，一天之內會武功全失，經脈逆流，胡思亂想而致走火入魔，最後會血管爆裂而死?", answer: "o"), Question(question: "吃了含笑半步癲的人，絕對不能走半步路或是面露笑容？", answer: "o"), Question(question: "唐伯虎的家傳絕學是情意綿綿劍？", answer: "x"), Question(question: "唐伯虎用絕妙畫技將祝枝山的雙掌作樹、屁股變高樓大廈？", answer: "x")]
+    var questions: [Question] = [
+                                 Question(question: "請問唐伯虎以西洋打擊樂器打動華夫人，最後終得順利成為書僮？", answer: "o"), 
+                                 Question(question:"華安在華太師府的終身代號是1314?", answer: "x"), 
+                                 Question(question: "對穿腸，綽號「對王之王」，名片上的職稱是七省文狀元兼參謀將軍？", answer: "o"),
+                                 Question(question: "含笑半步顛是用蜂蜜,川貝,桔梗,加上清新白蓮配製而成", answer: "x"), 
+                                 Question(question: "吃了一日喪命散的人，一天之內會武功全失，經脈逆流，胡思亂想而致走火入魔，最後會血管爆裂而死?", answer: "o"), 
+                                 Question(question: "吃了含笑半步癲的人，絕對不能走半步路或是面露笑容？", answer: "o"), 
+                                 Question(question: "唐伯虎的家傳絕學是情意綿綿劍？", answer: "x"), 
+                                 Question(question: "唐伯虎用絕妙畫技將祝枝山的雙掌作樹、屁股變高樓大廈？", answer: "x")
+                                 ]
     
     
     @IBOutlet weak var questionLable: UILabel!
